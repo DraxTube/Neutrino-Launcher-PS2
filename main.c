9b7a84766d5259c668d2459a98bff59e0e5246a4
DraxTube <draxtube01@gmail.com>
2026-01-16 21:03:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -17,9 +17,8 @@ int gameCount = 0;
 int selectedIndex = 0;
 int force_redraw = 1;
 
-// Configurazione basata sui tuoi TOML e sulla tua struttura
 #define NEUTRINO_PATH "mass:/neutrino.elf"
-#define GAMES_PREFIX  "mass1:/DVD/" // MX4SIO visto come mass1 dal launcher
+#define GAMES_PREFIX  "mass1:/DVD/" 
 
 static char padBuf[256] __attribute__((aligned(64)));
 
@@ -35,8 +34,7 @@ void init_system() {
 void scan_games() {
     gameCount = 0;
     DIR *dir = opendir(GAMES_PREFIX);
-    if (dir == NULL) dir = opendir("mass0:/DVD/"); // Fallback se mass1 fallisce
-    
+    if (dir == NULL) dir = opendir("mass0:/DVD/");
     if (dir != NULL) {
         struct dirent *ent;
         while ((ent = readdir(dir)) != NULL && gameCount < MAX_GAMES) {
@@ -52,32 +50,30 @@ void scan_games() {
 
 void launch_neutrino(char *isoName) {
     char iso_arg[256];
-    
-    // Per Neutrino, una volta caricato il driver mx4sio, 
-    // il dispositivo diventa 'mass:' (il bdm primario).
-    // Usiamo il formato esatto del README: -dvd=mass:path
+    // Specifichiamo il percorso completo incluso il device per evitare ambiguità
     sprintf(iso_arg, "-dvd=mass:/DVD/%s", isoName); 
 
-    char *args[6];
+    char *args[8];
     args[0] = NEUTRINO_PATH;
-    args[1] = "-bsd=mx4sio"; // Confermato dal tuo bsd-mx4sio.toml
-    args[2] = iso_arg;
-    args[3] = "-qb";         // Quick boot per evitare timeout grafici
-    args[4] = NULL;
+    args[1] = "-bsd=mx4sio"; 
+    args[2] = "-bsdfs=exfat"; // FORZIAMO exFAT come visto nel tuo toml
+    args[3] = iso_arg;
+    args[4] = "-cwd=mass:/";  // Forza la cartella di lavoro sulla USB per trovare i moduli
+    args[5] = "-qb";
+    args[6] = "-dbc";         // Attiva colori di debug: se vedi colori prima del crash, sappiamo dove fallisce
+    args[7] = NULL;
 
     scr_clear();
-    scr_printf("LANCIO IN CORSO...\n");
-    scr_printf("Utilizzo driver: %s\n", args[1]);
-    scr_printf("ISO target: %s\n", isoName);
-
-    // PULIZIA TOTALE: Neutrino v1.5+ deve avere l'IOP libero
+    scr_printf("LANCIO AVANZATO v2.4...\n");
+    
+    // Se la tua SD è FAT32, cambia args[2] in "-bsdfs=fatfs"
+    
     padPortClose(0,0);
     SifExitRpc();
     FlushCache(0);
     FlushCache(2);
 
-    // Carica Neutrino dalla USB (mass:) e passa il comando per la SD
-    LoadExecPS2(NEUTRINO_PATH, 4, args);
+    LoadExecPS2(NEUTRINO_PATH, 7, args);
 }
 
 int main() {
@@ -92,40 +88,24 @@ int main() {
         if (force_redraw) {
             scr_clear();
             scr_setfontcolor(0x00FFFF);
-            scr_printf("NEUTRINO LAUNCHER v2.3 - TOML READY\n");
+            scr_printf("NEUTRINO LAUNCHER v2.4 - DEBUG MODE\n");
             scr_printf("======================================\n\n");
-            
-            if (gameCount == 0) {
-                scr_printf("ERRORE: Nessuna ISO trovata in %s\n", GAMES_PREFIX);
-            } else {
-                scr_setfontcolor(0xFFFFFF);
+            if (gameCount == 0) scr_printf("ISO NON TROVATE!\n");
+            else {
                 for(int i = 0; i < gameCount; i++) {
-                    if (i == selectedIndex) {
-                        scr_setfontcolor(0x00FF00);
-                        scr_printf(" > %s\n", gameList[i]);
-                    } else {
-                        scr_setfontcolor(0xAAAAAA);
-                        scr_printf("   %s\n", gameList[i]);
-                    }
+                    if (i == selectedIndex) { scr_setfontcolor(0x00FF00); scr_printf(" > %s\n", gameList[i]); }
+                    else { scr_setfontcolor(0xAAAAAA); scr_printf("   %s\n", gameList[i]); }
                 }
             }
             force_redraw = 0;
         }
-
         if (padRead(0, 0, &buttons) != 0) {
             new_pad = 0xffff ^ buttons.btns;
-            if ((new_pad & PAD_DOWN) && !(old_pad & PAD_DOWN)) {
-                if (selectedIndex < gameCount - 1) { selectedIndex++; force_redraw = 1; }
-            }
-            if ((new_pad & PAD_UP) && !(old_pad & PAD_UP)) {
-                if (selectedIndex > 0) { selectedIndex--; force_redraw = 1; }
-            }
-            if ((new_pad & PAD_CROSS) && !(old_pad & PAD_CROSS)) {
-                if (gameCount > 0) launch_neutrino(gameList[selectedIndex]);
-            }
+            if ((new_pad & PAD_DOWN) && !(old_pad & PAD_DOWN)) { if (selectedIndex < gameCount - 1) { selectedIndex++; force_redraw = 1; } }
+            if ((new_pad & PAD_UP) && !(old_pad & PAD_UP)) { if (selectedIndex > 0) { selectedIndex--; force_redraw = 1; } }
+            if ((new_pad & PAD_CROSS) && !(old_pad & PAD_CROSS)) { if (gameCount > 0) launch_neutrino(gameList[selectedIndex]); }
             old_pad = new_pad;
         }
-        for(int i=0; i<30000; i++) asm("nop");
     }
     return 0;
 }
