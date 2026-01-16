65973ad4d62a915cf7fc6aef92d4f0a42c614163
DraxTube <draxtube01@gmail.com>
2026-01-16 01:23:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,46 +4,44 @@
 #include <sifrpc.h>
 #include "ds34usb_client.h"
 
-// Dati del driver incorporato (gestiti dal Makefile)
-extern unsigned char ds34usb_irx[];
-extern unsigned int size_ds34usb_irx;
+// Simboli creati automaticamente dal sistema
+extern unsigned char _binary_ds34usb_irx_start[];
+extern unsigned char _binary_ds34usb_irx_size[];
 
 int main() {
     SifInitRpc(0);
     init_scr();
     
-    // --- CREDITI DRAXTUBE ---
+    // --- CREDITI DraxTube ---
     scr_printf("==========================================\n");
     scr_printf("      NEUTRINO LAUNCHER - DraxTube        \n");
-    scr_printf("   GitHub: https://github.com/DraxTube    \n");
     scr_printf("   YouTube: @DraxTube01                   \n");
+    scr_printf("   GitHub: https://github.com/DraxTube    \n");
     scr_printf("==========================================\n\n");
     
-    scr_printf("Caricamento driver USB in corso...\n");
-    
-    // Carichiamo i moduli necessari della PS2
+    scr_printf("Inizializzazione USB...\n");
     SifLoadModule("rom0:SIO2MAN", 0, NULL);
     SifLoadModule("rom0:USBD", 0, NULL);
     
-    // Carichiamo il driver compilato nella memoria
-    SifExecModuleBuffer(ds34usb_irx, size_ds34usb_irx, 0, NULL, NULL);
+    // Carichiamo il driver integrato
+    unsigned int size = (unsigned int)_binary_ds34usb_irx_size;
+    SifExecModuleBuffer(_binary_ds34usb_irx_start, size, 0, NULL, NULL);
     
     if(ds34usb_init()) {
-        scr_printf("Stato: Driver PS3/PS4 ATTIVO!\n");
+        scr_printf("Stato: Controller PS3/PS4 PRONTO!\n");
     }
 
-    scr_printf("\nPremi il tasto CROCE per avviare il gioco...\n");
+    scr_printf("\nPremi CROCE per avviare il gioco...\n");
 
     u8 data[18];
     while(1) {
-        if (ds34usb_get_status(0) & 0x08) { // DS34USB_STATE_RUNNING
+        if (ds34usb_get_status(0) & 0x08) {
             ds34usb_get_data(0, data);
             if (data[5] & 0x40) { // Tasto Croce
-                scr_printf("Comando ricevuto! Lancio Neutrino...\n");
+                scr_printf("Avvio in corso...\n");
                 break;
             }
         }
     }
-
     return 0;
 }
