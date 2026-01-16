434e5036c41d026f67448f87a9a68e85f1f22196
DraxTube <draxtube01@gmail.com>
2026-01-16 01:16:57 +0100

    Update ds34usb_client.h

--- ds34usb_client.h
+++ ds34usb_client.h
@@ -1,10 +1,7 @@
 #ifndef DS34USB_CLIENT_H
 #define DS34USB_CLIENT_H
 #include <tamtypes.h>
-
-// Stati del controller
 #define DS34USB_STATE_RUNNING 0x08
-
 int ds34usb_init();
 int ds34usb_get_status(int port);
 int ds34usb_get_data(int port, u8 *data);
