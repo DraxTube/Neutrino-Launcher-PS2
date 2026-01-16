30b4df9bd6807df3ab2c416d2cc5bc7fd15cba7d
DraxTube <draxtube01@gmail.com>
2026-01-16 01:25:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
 #include <debug.h>
-#include <loadfile.h>
 #include <sifrpc.h>
-#include "ds34usb_client.h"
+#include <loadfile.h>
+#include <libpad.h>
 
-// Simboli creati automaticamente dal sistema
-extern unsigned char _binary_ds34usb_irx_start[];
-extern unsigned char _binary_ds34usb_irx_size[];
+// Buffer per il controller standard PS2
+static char padBuf[256] __attribute__((aligned(64)));
 
 int main() {
     SifInitRpc(0);
     init_scr();
-    
+
     // --- CREDITI DraxTube ---
     scr_printf("==========================================\n");
     scr_printf("      NEUTRINO LAUNCHER - DraxTube        \n");
     scr_printf("   YouTube: @DraxTube01                   \n");
     scr_printf("   GitHub: https://github.com/DraxTube    \n");
     scr_printf("==========================================\n\n");
-    
-    scr_printf("Inizializzazione USB...\n");
+
+    scr_printf("Caricamento moduli di sistema...\n");
     SifLoadModule("rom0:SIO2MAN", 0, NULL);
-    SifLoadModule("rom0:USBD", 0, NULL);
-    
-    // Carichiamo il driver integrato
-    unsigned int size = (unsigned int)_binary_ds34usb_irx_size;
-    SifExecModuleBuffer(_binary_ds34usb_irx_start, size, 0, NULL, NULL);
-    
-    if(ds34usb_init()) {
-        scr_printf("Stato: Controller PS3/PS4 PRONTO!\n");
-    }
+    SifLoadModule("rom0:PADMAN", 0, NULL);
 
-    scr_printf("\nPremi CROCE per avviare il gioco...\n");
+    padInit(0);
+    padPortOpen(0, 0, padBuf);
 
-    u8 data[18];
+    scr_printf("Sistema pronto. Usa un controller PS2 standard.\n");
+    scr_printf("Premi START per avviare Neutrino...\n");
+
+    struct padButtonStatus buttons;
     while(1) {
-        if (ds34usb_get_status(0) & 0x08) {
-            ds34usb_get_data(0, data);
-            if (data[5] & 0x40) { // Tasto Croce
-                scr_printf("Avvio in corso...\n");
-                break;
+        int state = padGetState(0, 0);
+        if (state == PAD_STATE_STABLE || state == PAD_STATE_FINDCTP1) {
+            int ret = padRead(0, 0, &buttons);
+            if (ret != 0) {
+                u32 paddata = 0xffff ^ buttons.btns;
+                if (paddata & PAD_START) {
+                    scr_printf("START premuto! Avvio in corso...\n");
+                    break;
+                }
             }
         }
     }
+
+    // Qui andrà il comando per lanciare Neutrino
     return 0;
 }
