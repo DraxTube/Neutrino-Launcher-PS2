32946cd2b4d96446764f1be99092ab98af8d2fd0
DraxTube <draxtube01@gmail.com>
2026-01-16 01:24:44 +0100

    Delete ds34usb_client.h

--- ds34usb_client.h
+++
@@ -1,8 +0,0 @@
-#ifndef DS34USB_CLIENT_H
-#define DS34USB_CLIENT_H
-#include <tamtypes.h>
-#define DS34USB_STATE_RUNNING 0x08
-int ds34usb_init();
-int ds34usb_get_status(int port);
-int ds34usb_get_data(int port, u8 *data);
-#endif
