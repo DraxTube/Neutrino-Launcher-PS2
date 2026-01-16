8cfb4eb164fabc83c0d68b53b61d8211dec2d9bc
DraxTube <draxtube01@gmail.com>
2026-01-16 01:01:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include <kernel.h>
 #include <sifrpc.h>
 #include <loadfile.h>
+#include <stdio.h>
 #include <gsKit.h>
 #include <dmaKit.h>
+#include <gsToolkit.h>
 #include <libpad.h>
 #include <dirent.h>
 #include <string.h>
+#include <unistd.h>
 #include "ds34usb.h"
 
-// Dati del driver incorporati (dal Makefile)
+// Simboli per il driver incorporato
 extern unsigned char ds34usb_irx[];
 extern unsigned int size_ds34usb_irx;
 
@@ -17,25 +20,24 @@ GSGLOBAL *gsGlobal;
 GSTEXTURE TexCover;
 char iso_list[30][128];
 int iso_count = 0, selected = 0;
+static char padBuf[256] __attribute__((aligned(64)));
 
-void load_all_modules() {
+void load_modules() {
     SifInitRpc(0);
     SifLoadModule("rom0:SIO2MAN", 0, NULL);
     SifLoadModule("rom0:PADMAN", 0, NULL);
-    // Carica il driver PS3/PS4 dalla memoria interna
+    // Carica il driver PS3/PS4 dalla memoria dell'ELF
     SifExecModuleBuffer(ds34usb_irx, size_ds34usb_irx, 0, NULL, NULL);
     ds34usb_init();
 }
 
 int main() {
-    load_all_modules();
+    load_modules();
     
-    // Inizializzazione Grafica GSKit
     gsGlobal = gsKit_init_global();
     dmaKit_init_all();
     gsKit_init_screen(gsGlobal);
     
-    // Scansione ISO
     DIR *d = opendir("mass:/DVD/");
     struct dirent *dir;
     while (d && (dir = readdir(d)) != NULL && iso_count < 30) {
@@ -46,28 +48,22 @@ int main() {
     if(d) closedir(d);
 
     while(1) {
-        gsKit_clear(gsGlobal, GS_SETREG_RGBAQ(0x00, 0x00, 0x20, 0x80, 0x00));
+        gsKit_clear(gsGlobal, GS_SETREG_RGBAQ(0x00, 0x00, 0x25, 0x80, 0x00));
         
-        // UI Testuale (Puoi migliorarla con i font)
-        printf("DraxTube Launcher - Seleziona Gioco\n");
-
-        // Lettura Input (Supporto DualShock 3/4 USB)
-        u8 ds_data[18];
+        // Logica di rendering (semplificata per stabilità)
         if (ds34usb_get_status(0) & DS34USB_STATE_RUNNING) {
-            ds34usb_get_data(0, ds_data);
-            // Esempio: Croce su DS4 (bitmask semplificata)
-            if (ds_data[5] & 0x40) break; 
+            u8 data[18];
+            ds34usb_get_data(0, data);
+            if (data[5] & 0x40) break; // Esempio: tasto Croce per avviare
         }
 
         gsKit_queue_exec(gsGlobal);
         gsKit_sync_flip(gsGlobal);
     }
 
-    // Lancio Neutrino
     char full_path[256];
     sprintf(full_path, "mass:/DVD/%s", iso_list[selected]);
     char *args[] = {"mass:/neutrino.elf", "-bs=mass", "-mod=dvd", full_path, NULL};
     execv(args[0], args);
-
     return 0;
 }
